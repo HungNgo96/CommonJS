e1f2a9b6bc3a8737585be404dc66ba83a51c40b1
Hung Ngo <hung.ngo@changeinteraction.com>
2020-12-28 14:01:24 +0700

    update angular9 html

--- File.js
+++ File.js
@@ -153,3 +153,16 @@ export const checkFileSize = (file, size = 5) => {
   if (fileSize > size * 1024) return false;
   return true;
 };
+
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+});
+
+async function Main() {
+   const file = document.querySelector('#myfile').files[0];
+   console.log(await toBase64(file));
+}
+
