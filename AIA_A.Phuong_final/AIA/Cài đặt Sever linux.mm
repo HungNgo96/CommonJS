30b47a84b7589e3ab507e2ffc310d80ab1295579
Hung Ngo <hung.ngo@changeinteraction.com>
2021-02-03 17:26:39 +0700

    update

---
+++ AIA_A.Phuong_final/AIA/Cài đặt Sever linux.mm
@@ -0,0 +1,68 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1611282792426" ID="ID_40019464" MODIFIED="1611282801095" TEXT="C&#xe0;i &#x111;&#x1eb7;t Sever linux">
+<node CREATED="1611282801845" ID="ID_1764877492" MODIFIED="1611282862839" POSITION="right" TEXT="1. H&#x1ec7; &#x111;i&#x1ec1;u h&#xe0;nh centos 7 64 bit">
+<node CREATED="1611555878959" ID="ID_1675127045" MODIFIED="1611555882020" TEXT="C&#xe0;i swap">
+<node CREATED="1611555900543" ID="ID_592124761" MODIFIED="1611555901307" TEXT="digitalocean.com/community/tutorials/how-to-add-swap-on-centos-7"/>
+</node>
+<node CREATED="1611555902207" ID="ID_371683778" MODIFIED="1611555904804" TEXT="C&#xe0;i wget"/>
+</node>
+<node CREATED="1611282866418" ID="ID_497323889" MODIFIED="1611282872166" POSITION="right" TEXT="2. C&#xe0;i &#x111;&#x1eb7;t ph&#x1ea7;n m&#x1ec1;m">
+<node CREATED="1611282818380" ID="ID_706296910" MODIFIED="1611282822534" TEXT="C&#xe0;i &#x111;&#x1eb7;t Nginx">
+<node CREATED="1611555566144" ID="ID_1439545339" MODIFIED="1611555568276" TEXT="C&#xe0;i &#x111;&#x1eb7;t"/>
+<node CREATED="1611282823066" ID="ID_1780342169" MODIFIED="1611282831695" TEXT="T&#x1ea1;o n&#x1a1;i l&#x1b0;u source">
+<node CREATED="1611555573187" ID="ID_469983376" MODIFIED="1611555580796" TEXT="T&#x1ea1;o th&#x1b0; m&#x1ee5;c /var/www"/>
+</node>
+<node CREATED="1611282832002" ID="ID_807410221" MODIFIED="1611282843334" TEXT="C&#x1ea5;u h&#xec;nh file nginx config cho domain">
+<node CREATED="1611283334924" ID="ID_1585450100" MODIFIED="1611283344399" TEXT="/etc/nginx/config.d/"/>
+<node CREATED="1611555583800" ID="ID_1485538787" MODIFIED="1611555593549" TEXT="T&#x1ea1;o t&#x1eeb;ng file config cho t&#x1eeb;ng domain &#x1edf; &#x111;&#xe2;y">
+<node CREATED="1611555594383" ID="ID_1218548670" MODIFIED="1611555615276" TEXT="Ex: api.anhduong.us.conf"/>
+</node>
+</node>
+</node>
+<node CREATED="1611282880043" ID="ID_1874863427" MODIFIED="1611282890895" TEXT="C&#xe0;i &#x111;&#x1eb7;t .NET Core SDK">
+<node CREATED="1611555646657" ID="ID_913843584" MODIFIED="1611555647365" TEXT="https://docs.microsoft.com/en-us/dotnet/core/install/linux-centos"/>
+<node CREATED="1611555686465" ID="ID_851199158" MODIFIED="1611555698476" TEXT="C&#xe0;i &#x111;&#x1eb7;t con CentOs 7 64 bit">
+<node CREATED="1611555699506" ID="ID_1423783122" MODIFIED="1611555701540" TEXT="SDK"/>
+<node CREATED="1611555701969" ID="ID_95217967" MODIFIED="1611555704012" TEXT="Runtime"/>
+</node>
+</node>
+<node CREATED="1611282892651" ID="ID_595472730" MODIFIED="1611283008270" TEXT="C&#xe0;i &#x111;&#x1eb7;t redis">
+<node CREATED="1611283009114" ID="ID_617832683" MODIFIED="1611283026924" TEXT="https://www.digitalocean.com/community/tutorials/how-to-install-secure-redis-centos-7"/>
+<node CREATED="1611283348481" ID="ID_1328746030" MODIFIED="1611283353630" TEXT="C&#x1ea5;u h&#xec;nh m&#x1eb7;c &#x111;&#x1ecb;nh">
+<node CREATED="1611283354092" ID="ID_1783084558" MODIFIED="1611283356422" TEXT="localhost"/>
+<node CREATED="1611283356778" ID="ID_455237227" MODIFIED="1611283361574" TEXT="port: 6379"/>
+<node CREATED="1611283361931" ID="ID_923352430" MODIFIED="1611283372637" TEXT="M&#x1edf; fiirewall cho port n&#x1ebf;u c&#xf3;"/>
+<node CREATED="1611283374138" ID="ID_835231791" MODIFIED="1611283378959" TEXT="&#x110;&#x1eb7;t mk cho redis"/>
+</node>
+</node>
+</node>
+<node CREATED="1611283389451" ID="ID_323952691" MODIFIED="1611283532982" POSITION="right" TEXT="3. Th&#x1ef1;c thi">
+<node CREATED="1611283533730" ID="ID_866423826" MODIFIED="1611283718525" TEXT="Ti&#x1ebf;n h&#xe0;nh upload source l&#xea;n folder"/>
+<node CREATED="1611283718876" ID="ID_127879411" MODIFIED="1611283737310" TEXT="C&#x1ea5;u h&#xec;nh file nginx config cho folder v&#xe0; domain t&#x1b0;&#x1a1;ng &#x1ee9;ng"/>
+<node CREATED="1611283737586" ID="ID_993071637" MODIFIED="1611283745102" TEXT="Ch&#x1ea1;y dotnet IconBe.dll">
+<node CREATED="1611283745996" ID="ID_1839712082" MODIFIED="1611283990094" TEXT="&#x110;&#x1ec3; ti&#x1ebf;n h&#xe0;nh ch&#x1ea1;y dotnet"/>
+</node>
+<node CREATED="1611283997946" ID="ID_889529824" MODIFIED="1611284026982" TEXT="C&#xf3; th&#x1ec3; vi&#x1ebf;t c&#x1ea5;u h&#xec;nh &#x111;&#x1ec3; dotnet t&#x1ef1; &#x111;&#x1ed9;ng ch&#x1ea1;y b&#xea;n d&#x1b0;&#x1edb;i, t&#x1ef1; &#x111;&#x1ed9;ng kh&#x1edf;i &#x111;&#x1ed9;ng l&#x1ea1;i sau khi fail"/>
+</node>
+<node CREATED="1611284045817" ID="ID_669482131" MODIFIED="1611284047830" POSITION="right" TEXT="L&#x1b0;u &#xfd;">
+<node CREATED="1611284048540" ID="ID_23350322" MODIFIED="1611284053726" TEXT="1. Qu&#xea;n m&#x1ead;t kh&#x1ea9;u root">
+<node CREATED="1611284054464" ID="ID_1421959896" MODIFIED="1611284062719" TEXT="V&#xe0;o droplet tr&#xea;n DO &#x111;&#x1ec3; reset"/>
+<node CREATED="1611284063209" ID="ID_593183437" MODIFIED="1611284067070" TEXT="V&#xe0;o email &#x111;&#x1ec3; xem"/>
+<node CREATED="1611284067321" ID="ID_788959685" MODIFIED="1611284074341" TEXT="&#x110;&#x1ed5;i mk n&#x1ebf;u &#x111;&#x103;ng nh&#x1ead;p l&#x1ea7;n &#x111;&#x1ea7;u"/>
+</node>
+<node CREATED="1611284075170" ID="ID_860273303" MODIFIED="1611284078495" TEXT="2. Upload file">
+<node CREATED="1611284079157" ID="ID_185845143" MODIFIED="1611284082622" TEXT="D&#xf9;ng filezilla"/>
+<node CREATED="1611284083993" ID="ID_1265562270" MODIFIED="1611284087277" TEXT="Port 22"/>
+<node CREATED="1611284087611" ID="ID_591201120" MODIFIED="1611284092094" TEXT="Giao th&#x1ee9;c SFTP"/>
+<node CREATED="1611284092378" ID="ID_1915693708" MODIFIED="1611284101373" TEXT="D&#xf9;ng tk qu&#x1ea3;n tr&#x1ecb; hay user &#x111;&#x1ec3; &#x111;&#x103;ng nh&#x1ead;p"/>
+</node>
+<node CREATED="1611284171170" ID="ID_627232825" MODIFIED="1611284184174" TEXT="3. N&#x1ebf;u mu&#x1ed1;n l&#xe0;m load balancing th&#xec; t&#x1ea1;o nhi&#x1ec1;u Droplet">
+<node CREATED="1611284190242" ID="ID_1115594107" MODIFIED="1611284193005" TEXT="T&#x1ea1;o droplet"/>
+<node CREATED="1611284193233" ID="ID_787857240" MODIFIED="1611284196423" TEXT="C&#xe0;i &#x111;&#x1eb7;t source"/>
+<node CREATED="1611284196705" ID="ID_460614482" MODIFIED="1611284200269" TEXT="C&#x1ea5;u h&#xec;nh nginx"/>
+<node CREATED="1611284207273" ID="ID_1633934324" MODIFIED="1611284211230" TEXT="S&#x1eed; d&#x1ee5;ng local ip"/>
+</node>
+</node>
+</node>
+</map>
